7019bde16164bcc00b2e735d06af089fe1fcd788
Athmika Senthilkumar <Athmika@Athmikas-MacBook-Pro.local>
2015-07-22 14:15:27 -0700

    22nd

---
+++ TemplateProject/Models/UserAlert.swift
@@ -0,0 +1,13 @@
+//
+//  UserAlert.swift
+//  TemplateProject
+//
+//  Created by Athmika Senthilkumar on 7/22/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
+//
+
+import UIKit
+
+class UserAlert: NSObject {
+   
+}
