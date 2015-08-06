1f611b957367b812c18d6fb083fd395f086f3df2
Athmika Senthilkumar <Athmika@Athmikas-MacBook-Pro.local>
2015-08-05 22:05:07 -0700

    Design

---
+++ TemplateProject/ViewControllers/LastViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LastViewController.swift
+//  TemplateProject
+//
+//  Created by Athmika Senthilkumar on 8/5/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
+//
+
+import Cocoa
+
+class LastViewController: UIViewController {
+
+}
